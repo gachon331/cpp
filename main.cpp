cd2eaf45b70bc0fbf636b3edaa058e604f9eb93a
lee je wook <80080537+gachon331@users.noreply.github.com>
2021-04-01 21:27:05 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+int main()
+{
+Car car1("GENESIS G70");
+car1.set_power();
+car1.set_speed(true);
+cout << car1.get_speed() << endl;
+return 0;
+
+Car car2("GENESIS G80");
+car2.set_power();
+car2.set_speed(true);
+cout << car2.get_speed() << endl;
+return 0;
+
+Car car3("GENESIS G90");
+car3.set_power();
+car3.set_speed(true);
+cout << car3.get_speed() << endl;
+return 0;
+
+Car car4("GENESIS GV80");
+car4.set_power();
+car4.set_speed(true);
+cout << car4.get_speed() << endl;
+return 0;
+}
