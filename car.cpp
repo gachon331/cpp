cad432b22d11ae797e2a1f6782970629a3a4476f
lee je wook <80080537+gachon331@users.noreply.github.com>
2021-04-01 21:26:49 +0900

    Add files via upload

---
+++ car.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Car{
+const string model;
+bool power;
+double speed;
+public:
+Car(string m, bool p=false, double s=0.0):model(m), power(p), speed(s){};
+void set_power();
+void set_speed(bool);
+double get_speed() const;
+};
+void Car::set_power()
+{
+power = !power;
+return;
+}
+void Car::set_speed(bool a)
+{
+if(!power)return;
+if(a)
+speed += 0.1;
+else
+speed -= 0.1;
+return;
+}
+double Car::get_speed() const
+{
+return speed;
+}
